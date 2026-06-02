13c70f96fc37efc98532bbcf4026efdb47736f8b
Chi-Chao Nim <Chi-Chao.Nim@tclarke.com>
2026-06-02 16:48:33 +0100

    Completed exercise 3.1-3.2

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,37 +1,40 @@
-import axios from "axios"
+import axios from "axios";
 
-const weatherAPIKey = import.meta.env.VITE_WEATHER_API_KEY 
+const weatherAPIKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-const baseCountryURL = "https://studies.cs.helsinki.fi/restcountries/api"
-const baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather?"
-const baseGeoLocalURL = "http://api.openweathermap.org/geo/1.0/direct?"
+const baseCountryURL = "https://studies.cs.helsinki.fi/restcountries/api";
+const baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather?";
+const baseGeoLocalURL = "http://api.openweathermap.org/geo/1.0/direct?";
 
 const getAllCountries = async () => {
-    try {
-        const request = axios.get(`${baseCountryURL}/all`)
-        return request.then(response => response.data)
-    } catch (error) {
-        console.error(error)
-    }
-    
-}
+  try {
+    const request = axios.get(`${baseCountryURL}/all`);
+    return request.then((response) => response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const getWeather = async (lat, long) => {
-    try {
-        const request = axios.get(`${baseWeatherURL}&lat=${lat}&lon=${long}&appid=${weatherAPIKey}&units=metric`)
-        return request.then(response => response.data)
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    const request = axios.get(
+      `${baseWeatherURL}&lat=${lat}&lon=${long}&appid=${weatherAPIKey}&units=metric`,
+    );
+    return request.then((response) => response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const getGeoLocal = async (cityName, countryCode) => {
-    try {
-        const request = axios.get(`${baseGeoLocalURL}q=${cityName},${countryCode}&limit=1&appid=${weatherAPIKey}`)
-        return request.then(response => response.data)
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    const request = axios.get(
+      `${baseGeoLocalURL}q=${cityName},${countryCode}&limit=1&appid=${weatherAPIKey}`,
+    );
+    return request.then((response) => response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export default {getAllCountries, getWeather, getGeoLocal}
+export default { getAllCountries, getWeather, getGeoLocal };
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+let people = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Phonebook Home</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>
+        <p>
+            Phonebook has info for ${people.length} people
+        </p>
+        <p>
+            ${new Date()}
+        </p>
+    </div>`,
+  );
+});
+
+app.get("/api/people", (request, response) => {
+  response.json(people);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
